661b4d536a62b302bf8b4448087515f367597e61
mikethecanuck <mikethecanuck@gmail.com>
2017-03-15 12:29:16 -0700

    experiment with Travis CLI *env* command - see if such env vars survive the script they're declared in

--- budget_proj/bin/test-proj.sh
+++ budget_proj/bin/test-proj.sh
@@ -6,6 +6,7 @@
 echo  Running test_proj.sh...
 
 echo DATABASE_PORT $DATABASE_PORT
+echo MIKE_ENV_VAR $MIKE_ENV_VAR
 
 # Run all configured unit tests inside the Docker container
 docker-compose -f budget_proj/docker-compose.yml run budget-service python manage.py test
