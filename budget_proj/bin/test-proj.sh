2ffead82bf0f47d7aa3d1c21c95d235765edfb33
mikethecanuck <mikethecanuck@gmail.com>
2017-03-16 09:17:35 -0700

    taking banzaiman advice - sourcing from .travis.yml

--- budget_proj/bin/test-proj.sh
+++ budget_proj/bin/test-proj.sh
@@ -18,7 +18,10 @@ echo DJANGO_SECRET_KEY-dummy $DJANGO_SECRET_KEY
 
 echo Setting key directly here to determine whether it is possible to ever pass the secret around as an env var...
 
-DJANGO_SECRET_KEY="m1o2gmv25hn0jny6xn3b3xo45zk70jw67dbef890mp1m03wm1d"
+#DJANGO_SECRET_KEY="m1o2gmv25hn0jny6xn3b3xo45zk70jw67dbef890mp1m012345"
+
+#echo Reading secret key back that I just set directly here
+#echo DJANGO_SECRET_KEY-dummy $DJANGO_SECRET_KEY
 
 # Run all configured unit tests inside the Docker container
 docker-compose -f budget_proj/docker-compose.yml run budget-service python manage.py test
